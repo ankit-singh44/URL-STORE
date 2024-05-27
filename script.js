1bb24b598769493fe834c4123c74f94696dbf9a7
Ankit Kumar Singh <70136925+ankit-singh44@users.noreply.github.com>
2024-05-27 16:08:07 +0530

    correcting few function naming convention and their Argument

--- script.js
+++ script.js
@@ -12,13 +12,27 @@ const leadsFromLocalStorage=JSON.parse(localStorage.getItem("myLeads"))
 
 if(leadsFromLocalStorage){
     myLeads=leadsFromLocalStorage
-    renderLead()
+    render(myLeads)
+}
+
+function render(leads){
+    let listItem="";
+    for(let i=0;i<leads.length;i++){
+        // listItem += "<li><a target='_blank' href='" +myLeads[i]+"'>"+myLeads[i]+"</a></li>";
+        listItem += `
+            <li>
+                <a target='_blank' href='${leads[i]}'>
+                    ${leads[i]}
+                </a>
+            </li>`;
+    }
+    ulel.innerHTML=listItem;
 }
 
 delbtn.addEventListener("click",function(){
     localStorage.clear();
     myLeads=[];
-    renderLead();
+    render(myLeads);
 })
 
 // console.log(leadsFromLocalStorage)
@@ -27,23 +41,11 @@ delbtn.addEventListener("click",function(){
 inputbtn.addEventListener("click",function(){
     myLeads.push(inputEl.value);
     inputEl.value='';
-    renderLead();
+    render(myLeads);
     localStorage.setItem("myLeads",JSON.stringify(myLeads))
     console.log(localStorage.getItem("myLeads"))
 })  
-function renderLead(){
-    let listItem="";
-    for(let i=0;i<myLeads.length;i++){
-        // listItem += "<li><a target='_blank' href='" +myLeads[i]+"'>"+myLeads[i]+"</a></li>";
-        listItem += `
-            <li>
-                <a target='_blank' href='${myLeads[i]}'>
-                    ${myLeads[i]}
-                </a>
-            </li>`;
-    }
-    ulel.innerHTML=listItem;
-}
+
 
 
 
